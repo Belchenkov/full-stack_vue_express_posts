72b5ddb7834718c4fb53ae9d3a827de453d6058f
Алексей Бельченков <u608110@gmail.com>
2018-12-21 13:23:55 +0400

    Connect to MongoDB and Get All Posts

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,13 +4,23 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get Posts
-router.get('/', (req, res) => {
-    res.send('Hello');
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
 });
 
 // Add Post
 
 
 // Delete Post
+
+async function loadPostsCollection () {
+    const client =
+        await mongodb.MongoClient
+            .connect('mongodb://abc123:abc123@ds253918.mlab.com:53918/vue_express_full_stack_brad', {
+                useNewUrlParser: true
+            });
+    return client.db('vue_express_full_stack_brad').collection('posts');
+}
 
 module.exports = router;
